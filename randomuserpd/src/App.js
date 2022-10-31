200259842c2753c67ae5631835655d783e4ed172
Ben Pluim <benpluim@Bens-MacBook-Pro.local>
2022-10-31 12:13:43 -0600

    added cards and fetch api

--- randomuserpd/src/App.js
+++ randomuserpd/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import InputUnstyled from '@mui/base/InputUnstyled';
+import Card from '@mui/material/Card';
 
 function App() {
+  const [members, setMembers] = useState();
+  const [search, setSearch] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=20")
+    .then((response) => response.json())
+    .then((response) => {
+      setMembers(response.results)
+      setLoading(true);
+    })
+  }, [])
+
+  console.log({members})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height: '100%',background: "282c34"}}>
+      <AppBody>
+        <InputUnstyled placeholder="search" value={search} onChange={(e) => setSearch(e.target.value)}/>
+        {!loading ? 
+          <h3>Loading...</h3>
+          :
+          <CardsContainer>
+            {members.map(member => (
+              <Card sx={{ width: 200, textAlign: 'center', margin: '15px 5px' }} key={member.login.uuid}>
+                <MemberImage src={member.picture.large} />
+                <p>{member.name.first} {member.name.last}</p>
+                <p>Age: {member.dob.age}</p>
+              </Card>
+            ))}
+          </CardsContainer>
+        }
+      </AppBody>
     </div>
   );
 }
+
+const AppBody = styled.div({
+  height: '100%',
+  padding: '10% 20%',
+  background: '#282c34',
+  color: '#fff'
+})
+
+const CardsContainer = styled.div({
+  marginTop: 30,
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  flexFlow: 'row wrap'
+})
+
+const MemberImage = styled.img({
+  borderRadius: '50%',
+  border: 'solid 2px #282c34',
+})
 
 export default App;
